8940f3bd956e5360e6ad3d7e3548ce709ace85d3
Isaac Rodríguez <edgar.rodriguez@docsolutions.com>
2025-01-22 13:44:25 -0700

    prueba

--- SistemaMedico.Api/Startup.cs
+++ SistemaMedico.Api/Startup.cs
@@ -28,7 +28,6 @@ namespace SistemaMedico.Api
             Configuration = configuration;
         }
 
-
         public IConfiguration Configuration { get; }
 
         // This method gets called by the runtime. Use this method to add services to the container.
